d5c1aa5222d79c74c9512770e09f3a7fcc89d29a
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2020-06-28 16:57:45 +0200

    Built on OpenBSD.

--- config.mk
+++ config.mk
@@ -13,6 +13,9 @@ MANPREFIX = $(PREFIX)/share/man
 # X11 API installation paths (needed by sftdemo)
 X11INC = /usr/include/X11
 X11LIB = /usr/lib/X11
+# Uncomment below to build sftdemo on OpenBSD
+# X11INC = /usr/X11R6/include
+# X11LIB = /usr/X11R6/lib
 
 # flags
 CPPFLAGS = -I./

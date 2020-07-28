63d97d8bca5ea8d54deeba5622a06bd1e05d553f
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2020-07-28 14:29:35 +0200

    Fixed careless integer conversions in demos & utils.

--- util/utf8_to_utf32.h
+++ util/utf8_to_utf32.h
@@ -12,32 +12,32 @@ utf8_to_utf32(const uint8_t *utf8, uint32_t *utf32, int max)
 	while (*utf8) {
 		if (i >= max)
 			return 0;
-		if (!(*utf8 & 0x80)) {
+		if (!(*utf8 & 0x80U)) {
 			utf32[i++] = *utf8++;
-		} else if ((*utf8 & 0xe0) == 0xc0) {
-			c = (*utf8++ & 0x1f) << 6;
-			if ((*utf8 & 0xc0) != 0x80) return 0;
-			utf32[i++] = c + (*utf8++ & 0x3f);
-		} else if ((*utf8 & 0xf0) == 0xe0) {
-			c = (*utf8++ & 0x0f) << 12;
-			if ((*utf8 & 0xc0) != 0x80) return 0;
-			c += (*utf8++ & 0x3f) << 6;
-			if ((*utf8 & 0xc0) != 0x80) return 0;
-			utf32[i++] = c + (*utf8++ & 0x3f);
-		} else if ((*utf8 & 0xf8) == 0xf0) {
-			c = (*utf8++ & 0x07) << 18;
-			if ((*utf8 & 0xc0) != 0x80) return 0;
-			c += (*utf8++ & 0x3f) << 12;
-			if ((*utf8 & 0xc0) != 0x80) return 0;
-			c += (*utf8++ & 0x3f) << 6;
-			if ((*utf8 & 0xc0) != 0x80) return 0;
-			c += (*utf8++ & 0x3f);
-			if ((c & 0xFFFFF800) == 0xD800) return 0;
-			if (c >= 0x10000) {
-				c -= 0x10000;
+		} else if ((*utf8 & 0xe0U) == 0xc0U) {
+			c = (*utf8++ & 0x1fU) << 6;
+			if ((*utf8 & 0xc0U) != 0x80U) return 0;
+			utf32[i++] = c + (*utf8++ & 0x3fU);
+		} else if ((*utf8 & 0xf0U) == 0xe0U) {
+			c = (*utf8++ & 0x0fU) << 12;
+			if ((*utf8 & 0xc0U) != 0x80U) return 0;
+			c += (*utf8++ & 0x3fU) << 6;
+			if ((*utf8 & 0xc0U) != 0x80U) return 0;
+			utf32[i++] = c + (*utf8++ & 0x3fU);
+		} else if ((*utf8 & 0xf8U) == 0xf0U) {
+			c = (*utf8++ & 0x07U) << 18;
+			if ((*utf8 & 0xc0U) != 0x80U) return 0;
+			c += (*utf8++ & 0x3fU) << 12;
+			if ((*utf8 & 0xc0U) != 0x80U) return 0;
+			c += (*utf8++ & 0x3fU) << 6;
+			if ((*utf8 & 0xc0U) != 0x80U) return 0;
+			c += (*utf8++ & 0x3fU);
+			if ((c & 0xFFFFF800U) == 0xD800U) return 0;
+			if (c >= 0x10000U) {
+				c -= 0x10000U;
 				if (i + 2 > max) return 0;
-				utf32[i++] = 0xD800 | (0x3ff & (c >> 10));
-				utf32[i++] = 0xDC00 | (0x3ff & (c      ));
+				utf32[i++] = 0xD800U | (0x3ffU & (c >> 10));
+				utf32[i++] = 0xDC00U | (0x3ffU & (c      ));
 			}
 		} else return 0;
 	}

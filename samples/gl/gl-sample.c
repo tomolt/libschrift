55f81e6a7bc8480e023949f5826fbf3aaec5ae92
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2023-03-14 21:01:36 +0100

    Populating the GL VBO

--- samples/gl/gl-sample.c
+++ samples/gl/gl-sample.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <epoxy/gl.h>
 #include <GLFW/glfw3.h>
@@ -14,10 +15,10 @@
 
 static const char *vertex_shader_source[] = {
 	"#version 330 core\n",
-	"uniform mat4 transform;",
+	//"uniform mat4 transform;",
 	"in vec2 vert_pos;",
 	"void main() {",
-	"	gl_Position = transform * vec4(vert_pos, 0.0f, 1.0f);",
+	"	gl_Position = vec4(vert_pos, 0.0f, 1.0f);",
 	"}"
 };
 
@@ -34,6 +35,7 @@ static GLuint shader;
 static GLuint vao;
 static GLuint vbo;
 static GLuint atlas;
+static SFT sft;
 
 void die(const char *msg)
 {
@@ -53,7 +55,8 @@ void init_gl(void)
 	glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
 
 	// Compile shaders
-	GLuint vert, frag, status;
+	GLuint vert, frag;
+	GLint status;
 	vert = glCreateShader(GL_VERTEX_SHADER);
 	frag = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(vert, LENGTH(vertex_shader_source), vertex_shader_source, NULL);
@@ -94,28 +97,61 @@ void init_gl(void)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
+void draw_text(const char *text)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, VBO_SIZE, NULL, GL_STREAM_DRAW); // Orphan old buffer contents
+	float *coords = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	
+	float *pointer = coords;
+	size_t i;
+	float penX = 0.0f;
+
+	for (i = 0; text[i]; i++) {
+
+		SFT_Glyph glyph;
+		if (sft_lookup(&sft, text[i], &glyph) < 0)
+			die("Can't look up glyph id");
+		SFT_GMetrics gmtx;
+		if (sft_gmetrics(&sft, glyph, &gmtx) < 0)
+			die("Can't look up glyph metrics");
+
+		float x1 = penX + gmtx.leftSideBearing / 320.f;
+		float y1 = gmtx.yOffset / 240.f;
+		float x2 = x1 + gmtx.minWidth / 320.f;
+		float y2 = y1 + gmtx.minHeight / 240.f;
+
+		penX += gmtx.advanceWidth / 320.f;
+
+		float glyph_pos[] = {x1, y1, x1, y1, x2, y1, x1, y2, x2, y2, x2, y2};
+		memcpy(pointer, glyph_pos, sizeof glyph_pos);
+		pointer += LENGTH(glyph_pos);
+	}
+
+	glUnmapBuffer(GL_ARRAY_BUFFER);
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLE_STRIP, 1, 6 * i - 2);
+}
+
 int main()
 {
 	init_gl();
 
-	SFT sft = { 0 };
 	sft.font = sft_loadfile("../../resources/fonts/FiraGO-Regular.ttf");
 	if (!sft.font)
 		die("Can't load font");
 	sft.xScale = 16;
 	sft.yScale = 16;
-	sft.flags = SFT_DOWNWARD_Y;
 	
 	SFT_LMetrics lmtx;
 	if (sft_lmetrics(&sft, &lmtx) < 0)
 		die("Can't query line metrics");
 
-	float penX = 0.0f, penY = 0.0f;
-	SFT_Glyph prevGlyph;
-
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader);
+		draw_text("Lorem ipsum dolor sit amet");
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
